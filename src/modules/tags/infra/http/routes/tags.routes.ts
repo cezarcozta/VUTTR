68dbaf62922ce606ea34623aab57d4eed3f02c63
César Augusto Costa <cezarcozta@gmail.com>
2020-06-29 11:47:00 -0300

    feat: middleware ensureAuthenticated

--- src/modules/tags/infra/http/routes/tags.routes.ts
+++ src/modules/tags/infra/http/routes/tags.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+
 import TagsController from '../controllers/TagsController';
 
 const tagsRouter = Router();
 const tagsController = new TagsController();
+
+tagsRouter.use(ensureAuthenticated);
 
 tagsRouter.post('/', tagsController.create);
 
--- src/modules/tools/infra/http/routes/tools.routes.ts
+++ src/modules/tools/infra/http/routes/tools.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+
 import ToolsController from '@modules/tools/infra/http/controllers/ToolsController';
 
 const toolsRouter = Router();
 const toolsController = new ToolsController();
+
+toolsRouter.use(ensureAuthenticated);
 
 toolsRouter.post('/', toolsController.create);
 
